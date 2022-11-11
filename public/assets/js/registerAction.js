3277ff41a878e662904b441244e116dd8efb584d
Bokitreks <51173040+Bokitreks@users.noreply.github.com>
2022-11-11 12:00:42 +0100

    Resolved some conflicts

--- public/assets/js/registerAction.js
+++ public/assets/js/registerAction.js
@@ -18,18 +18,11 @@ function registerAction() {
             'password': password,
             'email': email
         },
-<<<<<<< HEAD
-        success:function()
-
-
-=======
         success:function(data) {
             alert(data);
         },
         error:function(xhr,status,error) {
             console.log(xhr);
         }
-        
->>>>>>> ef8e56c60f00f081d71a857d5046128adff8b86b
     });
 }
